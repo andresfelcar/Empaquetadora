3caa02059f192331bedc6aa32022dfc42602a5a1
diego-034 <diego-034>
2020-05-18 11:18:56 -0500

    Crando rama

--- model/Conexion.php
+++ model/Conexion.php
@@ -4,7 +4,7 @@ class Conexion{
     private function __construct(){}
 
     public static function connection(){
-        return mysqli_connect("localhost", "u539108239_laimperial", "[~*MjfF6", "u539108239_laimperial");
+        return mysqli_connect("localhost", "root", "", "u539108239_laimperial");
     }  
 }
     
